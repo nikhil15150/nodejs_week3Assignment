dc08aaeb275569ac7489e2a1e9cb9a81c179d360
nikhil <nikhil15150@iiitd.ac.in>
2020-05-05 19:38:12 +0530

    signed cookies added

--- app.js
+++ app.js
@@ -32,37 +32,59 @@ connect.then((db)=>
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//implement basic authentication
+//implement basic authentication with cookies
+app.use(cookieParser('12345-67890-09876-54321'));
 function auth(req,res,next)
 {
   console.log(req.headers);
-  var authHeader=req.headers.authorization;
-  if(!authHeader)
+  if(!req.signedCookies.user)
   {
-    var err=new Error('Your are not authenticated');
-    res.setHeader('www-authotication','Basic');
-    err.statuscode=401;
-    next(err);
-    return;
+      var authHeader=req.headers.authorization;
+      if(!authHeader)
+      {
+        var err=new Error('Your are not authenticated');
+        res.setHeader('www-authetication','Basic');
+        err.statuscode=401;
+        next(err);
+        return;
+      }
+      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      var user=auth[0];
+      var password=auth[1];
+      if(user==='admin' && password==='password')
+      {
+        res.cookie('user','admin',{signed: true});
+        //res.cookie('user','admin',{signed:true});
+        next();
+      }
+      else
+      {
+        var err=new Error('Your are not authenticated');
+        res.setHeader('www-authetication','Basic');
+        err.statuscode=401;
+        next(err);
+        return;
+      }
+    }
+    else{
+      if(req.signedCookies.user==='admin')
+      {
+        next();
+      }
+      else
+      {
+        var err=new Error('Your are not authenticated');
+        res.setHeader('www-authotication','Basic');
+        err.statuscode=401;
+        next(err);
+        return;
+
+      }
+    }
   }
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user=auth[0];
-  var password=auth[1];
-  if(user==='admin' && password==='password')
-  {
-    next();
-  }
-  else
-  {
-    var err=new Error('Your are not authenticated');
-    res.setHeader('www-authotication','Basic');
-    err.statuscode=401;
-    next(err);
-    return;
-  }
-}
+
+  
 
 app.use(auth);
 
